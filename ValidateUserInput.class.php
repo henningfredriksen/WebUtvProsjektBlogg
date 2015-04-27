3293341a852c272597ae6b63b7425a9ba09ad651
silje hauknes <lillesith@gmail.com>
2015-04-27 22:15:42 +0200

    Create ValidateUserInput.class.php

---
+++ ValidateUserInput.class.php
@@ -0,0 +1,14 @@
+<?php
+
+class ValidateUserInput {
+	
+	public function __construct() {
+		
+	}
+	
+	public function validateInputString($input) {
+		$validatedinput = strip_tags($input);
+		$validatedinput = trim($validatedinput);
+		return $validatedinput;
+	}
+}
