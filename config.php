efe5459fecf68546f34b0ac74e8052b8e4191097
henningfredriksen <rhymfaxe2@hotmail.com>
2015-05-07 14:50:19 +0200

    config

--- config.php
+++ config.php
@@ -1,10 +1,9 @@
 <?php
-require 'libs/Smarty.class.php';
+require 'libs' . DIRECTORY_SEPARATOR . 'Smarty.class.php';
 session_start();
 $smarty = new Smarty();
 
 function siljesAutoloader($class_name){
-	// __DIR__ . DIRECTORY_SEPARATOR.
 	include $class_name . '.class' . '.php';
 }
 
