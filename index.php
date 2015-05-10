b21d9e43aa480ac2f6c629aa6343f1116a3c0f95
henningfredriksen <rhymfaxe2@hotmail.com>
2015-05-10 03:00:28 +0200

    dfgdfgdf

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title> Index
+</title>
+</head>
 <?php
 require_once 'config.php';
 
@@ -115,3 +123,4 @@ if(isset($_SESSION['filesizeerror']))
 
 $smarty->display('index.tpl');
 ?>
+</html>
